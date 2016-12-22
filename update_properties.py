5b362d1e9cfb47874f63f8262852756431c9295a
yaming116 <yaming116@163.com>
2016-12-22 16:25:19 +0800

    add test

--- update_properties.py
+++ update_properties.py
@@ -10,7 +10,7 @@ import re
 import time
 
 
-def update_properties(config_json_data, props_path, config_apk, verbose = False):
+def update_properties(config_json_data, props_path, config_apk, test, verbose = False):
 
     if verbose:
         print 'start update properties'
@@ -20,15 +20,26 @@ def update_properties(config_json_data, props_path, config_apk, verbose = False)
 
     for key, value in config_json_data.items():
         data = re.sub(pattern % key, r'%s = %s' % (key, value), data)
-
+    http_path = config_json_data.get('TEST_HTTP_PATH', None)
     apk_path = config_json_data.get('APK_PATH')
+
+    if test :
+        if http_path and len(http_path) > 0:
+            key = u'HTTP_PATH'
+            value = http_path
+            data = re.sub(pattern % key, r'%s = %s' % (key, value), data)
+        else:
+            raise ValueError('test http path not found')
 
     localtime = time.localtime(time.time())
     day = time.strftime("%Y-%m-%d", time.localtime())
     id = int(time.mktime(localtime) / 10)
-    ipa_name = '%s_%s.apk' % (day, id)
+    if test:
+        ipa_name = '%s_%s_test.apk' % (day, id)
+    else:
+        ipa_name = '%s_%s.apk' % (day, id)
 
-    if apk_path == None or len(apk_path) <= 0 :
+    if not apk_path or len(apk_path) <= 0 :
         value = os.path.join(config_apk, ipa_name)
         key = u'APK_PATH'
         config_json_data[key] = value
