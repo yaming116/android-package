6e8534e24a2fabac62469ac716e537468c4273dd
孙善明 <sunshanming@zhuojianchina.com>
2019-02-28 14:45:10 +0800

    添加隐藏就诊记录配置

--- update_config.py
+++ update_config.py
@@ -57,9 +57,9 @@ def update_plist_option(options, config_path, source, resource, verbose):
         update_config('HOSPITAL_AREA', 'false', config_path, verbose)
 
     if options.has_key('IS_OPEN_RECORD_LIST') and options['IS_OPEN_RECORD_LIST']:
-        update_config('IS_OPEN_RECORD_LIST', 'true', config_path, verbose)
-    else:
         update_config('IS_OPEN_RECORD_LIST', 'false', config_path, verbose)
+    else:
+        update_config('IS_OPEN_RECORD_LIST', 'true', config_path, verbose)
 
     if options.has_key('HOSPITAL_URL'):
         r = '"%s"' % options['HOSPITAL_URL']
